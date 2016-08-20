7016a025aaca33f58d9ce041f735b47189719d1a
igor <igor@klopov.com>
2016-08-20 10:19:08 +0300

    apply marker to babel-runtime/core-js/promise

--- empty.js
+++ empty.js
@@ -1,4 +1,14 @@
-Promise.marker = {};
-Promise.marker.mark = function(p) { return p; };
-Promise.marker.topmostAsync = function(p) { return p; };
-Promise.marker.checkMarkList = function() {};
+function append(promise) {
+  promise.marker = {};
+  promise.marker.mark = function(p) { return p; };
+  promise.marker.topmostAsync = function(p) { return p; };
+  promise.marker.checkMarkList = function() {};
+}
+
+append(Promise);
+
+try {
+  const Promise2 = require("babel-runtime/core-js/promise").default;
+  append(Promise2);
+} catch (_) {
+}
